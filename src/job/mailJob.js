14d63986d6e16ef4853be02363e6c950f55851e9
linhaobo <boris890221@gmail.com>
2023-03-09 09:30:51 +0900

    try to fix glitch node error: change private field to normal field

--- src/job/mailJob.js
+++ src/job/mailJob.js
@@ -1,27 +1,27 @@
 class MailJob {
-  #delay = 0;
-  #mailSender = null;
+  delay = 0;
+  mailSender = null;
 
   constructor(delay, mailSender) {
-    this.#delay = delay;
-    this.#mailSender = mailSender
+    this.delay = delay;
+    this.mailSender = mailSender
   }
 
   start() {
-    if(!this.#mailSender || this.#delay === 0) {
+    if(!this.mailSender || this.delay === 0) {
       console.log("jos start failed");
       return;
     }
 
-    if (!this.#mailSender.send) {
+    if (!this.mailSender.send) {
       console.log("please implement [sendMail] in service");
       return;
     }
 
     console.log("jos start.");
     setInterval(() => {
-      this.#mailSender.send();
-    }, this.#delay * 1000);
+      this.mailSender.send();
+    }, this.delay * 1000);
   }
 }
 
